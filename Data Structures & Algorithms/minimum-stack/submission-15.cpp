a89e8b5266e4d064a8650617e24d9c153b973ccd
Avinash57r <159982469+Avinash57r@users.noreply.github.com>
2026-05-13 01:41:08 -0400

    Add: minimum-stack - submission-15

---
+++ Data Structures & Algorithms/minimum-stack/submission-15.cpp
@@ -0,0 +1,36 @@
+#include <stack>
+class MinStack {
+public:
+
+    stack<int> mainStack;
+    stack<int> minStack;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(minStack.size() == 0)
+        {
+            minStack.push(val);
+        } else if(val < minStack.top() || val == minStack.top()){
+            minStack.push(val);
+        }
+        mainStack.push(val);
+    }
+    
+    void pop() {
+        if(mainStack.top() == minStack.top())
+        {
+            minStack.pop();
+        }
+        mainStack.pop();
+    }
+    
+    int top() {
+        return mainStack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
