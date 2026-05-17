e7046395a330ca69395bab7d2e27cc30507f8fcb
Avinash57r <159982469+Avinash57r@users.noreply.github.com>
2026-05-16 23:53:54 -0400

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int leftMatrix = 0;
+        int rightMatrix = matrix.size() -1;
+        int rowLength = matrix[0].size() -1;
+
+        while(leftMatrix <= rightMatrix)
+        {
+            int midMatrix = leftMatrix + ((rightMatrix - leftMatrix) /2); //find mid
+            for(const int &element: matrix[midMatrix])
+            {
+                if (element == target) {return true;}
+            }
+            //get rid of right
+            if(target < matrix[midMatrix][0]) {rightMatrix = midMatrix -1;}
+            //get rid of left side
+            else if(target > matrix[midMatrix][rowLength]) {leftMatrix = midMatrix +1;}
+            else {return false;}
+        }
+        return false;
+        
+    }
+};
