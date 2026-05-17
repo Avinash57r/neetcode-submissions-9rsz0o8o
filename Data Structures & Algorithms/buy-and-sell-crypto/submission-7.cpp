8fa95d8026be28840df3c00b8b22c96a71956871
Avinash57r <159982469+Avinash57r@users.noreply.github.com>
2026-05-17 02:18:01 -0400

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int leftIndex{0};
+        int rightIndex{1};
+        int maxProfit{0};
+
+        while(rightIndex < prices.size())
+        {
+            if(prices[rightIndex] < prices[leftIndex]){leftIndex = rightIndex;}
+
+            if(maxProfit < prices[rightIndex] - prices[leftIndex])
+            {
+                maxProfit = prices[rightIndex] - prices[leftIndex];
+            }
+
+            rightIndex++;
+        }
+        return maxProfit;
+        
+    }
+};
