09e7272bc925bd0e18bb40e5efc0deb36c1c85c1
Avinash57r <159982469+Avinash57r@users.noreply.github.com>
2026-08-08 00:17:38 -0400

    Add: validate-parentheses - submission-19

---
+++ Data Structures & Algorithms/validate-parentheses/submission-19.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        if(s.length() % 2 != 0) {return false;}
+
+        std::vector<char> stack;
+        stack.reserve(s.length()/2);
+
+        for(char& c : s)
+        {
+            if(c == '(') {stack.push_back(')');}
+            else if(c == '{') {stack.push_back('}');}
+            else if(c == '[') {stack.push_back(']');}
+            else 
+            {
+                if (stack.empty() || stack.back() != c) {return false;}
+                stack.pop_back();
+            }
+        }
+
+        return(stack.empty());
+        
+    }
+};
